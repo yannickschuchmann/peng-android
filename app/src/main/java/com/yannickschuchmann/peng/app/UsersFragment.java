153168cc9f5b9bc20a347414ac098db15259abd0
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2015-06-29 20:49:42 +0200

    added paddingTop as actionBarSize to activity layouts created ranking activity with users listing fragment

---
+++ app/src/main/java/com/yannickschuchmann/peng/app/UsersFragment.java
@@ -0,0 +1,19 @@
+package com.yannickschuchmann.peng.app;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class UsersFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_users, container, false);
+    }
+}
