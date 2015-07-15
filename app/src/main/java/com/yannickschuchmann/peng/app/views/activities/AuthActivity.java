a9f44ea6a5ac54874b96035106404933bb055c93
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2015-07-15 20:07:29 +0200

    fixed title added icon

--- app/src/main/java/com/yannickschuchmann/peng/app/views/activities/AuthActivity.java
+++ app/src/main/java/com/yannickschuchmann/peng/app/views/activities/AuthActivity.java
@@ -35,6 +35,7 @@ public class AuthActivity extends TransitionActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+        setTitle(R.string.app_name);
         int userId = CurrentUser.getInstance(getContext()).getUserId();
         if (userId != 0) {
             Intent intent = new Intent(getContext(), MainActivity.class);
